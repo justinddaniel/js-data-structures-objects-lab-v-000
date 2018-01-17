0b6b456bc96cfc23e49351bc1ab83b51e3cf182b
Justin Daniel <justinddaniel@gmail.com>
2018-01-17 00:55:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,8 @@ function deleteFromDriverByKey (driver, key) {
   delete newdriver[key];
   return newdriver;
 }
+
+function destructivelyDeleteFromDriverByKey (driver, key) {
+  delete driver[key];
+  return driver;
+}
