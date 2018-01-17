ed4fe1e4b6a8fc1d4047951c5442fbe282b07212
Justin Daniel <justinddaniel@gmail.com>
2018-01-17 00:54:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function destructivelyUpdateDriverWithKeyAndValue (driver, key, value) {
 
 function deleteFromDriverByKey (driver, key) {
   const newdriver = {...driver};
-  delete newdriver.key;
+  delete newdriver[key];
   return newdriver;
 }
